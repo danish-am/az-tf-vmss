dea57ecc55fcd1735c4df2843d943fe643c8d91d
Danish A M <41690164+danish-am@users.noreply.github.com>
2025-04-20 16:19:28 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name   = "terraform-backend-rg"
     storage_account_name  = "tfbackenddanish123"
-    container_name        = "tfstate"  # Confirm this name in the Azure portal
+    container_name        = "tfstate"  
     key                   = "vmssapp.terraform.tfstate"
   }
 }
